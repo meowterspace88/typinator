b2770a33677b049087f3cf448cf3bf586f43542f
Alexandra <little.runyan@gmail.com>
2017-11-06 13:02:41 -0800

    test conversaion

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// const fs = require('fs');
-// const https = require('https');
-// const request = require('request');
-// const express = require('express');
-// const formidable = require('formidable');
-// const removeMd = require('remove-markdown');
+// const fs = require("fs");
+// const https = require("https");
+// const request = require("request");
+// const express = require("express");
+// const formidable = require("formidable");
+const removeMd = require("remove-markdown");
 // const server = http.createServer();
 const form = document.getElementById("form1");
 const input = form.querySelector("input");
-// const form2 = document.getElementById('form2');
-// const plainText = removeMd(markdown);
+const form2 = document.getElementById("form2");
 
 form.onchange = function convertMarkdown() {
   e.preventDefault();
   const markdown = input.value;
-  const form2 = document.getElementById("form2");
-  form2.textContent = this.markdown;
+  const plainText = removeMd(markdown);
+  form2.textContent = plainText;
 };
 
 
