64d6b1f109c37c45b025c68b27cfd19823c19fb9
Alexandra <little.runyan@gmail.com>
2017-11-06 12:53:48 -0800

    another fix

--- app.js
+++ app.js
@@ -5,23 +5,17 @@
 // const formidable = require('formidable');
 // const removeMd = require('remove-markdown');
 // const server = http.createServer();
-const form = document.getElementById('form1');
-const input = form.querySelector('input');
+const form = document.getElementById("form1");
+const input = form.querySelector("input");
 // const form2 = document.getElementById('form2');
 // const plainText = removeMd(markdown);
 
-form.addEventListener("submit", (e) => {
+form.onchange = function convertMarkdown() {
   e.preventDefault();
   const markdown = input.value;
-  const form2 = document.getElementById('form2');
-  form2.textContent = markdown;
-  // markdown.textContent = text;
-});
-
-// form2.addEventListener("input", (e) => {
-//   e.preventDefault();
-//   // plainText
-// });
+  const form2 = document.getElementById("form2");
+  form2.textContent = this.markdown;
+};
 
 
 // server.on('request', (request, response) => {
